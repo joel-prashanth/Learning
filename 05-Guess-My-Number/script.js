2d99ac24a247e784e5162a94bb9c7930fe01a6ef
Joel Prashanth <142143829+joel-prashanth@users.noreply.github.com>
2025-03-16 22:08:54 +0530

    Refactor code

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -12,6 +12,10 @@
 
 let score = 20;
 let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.score').textContent = score;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = '?';
@@ -21,12 +25,19 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     //NO INPUT
-    document.querySelector('.message').textContent =
-      'Enter a Number first...🤷‍♂️';
-    //WIN
-  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent =
+    //   'Enter a Number first...🤷‍♂️';
+
+    displayMessage('Enter a Number first...🤷‍♂️');
+  }
+
+  //WIN
+  else if (guess === secretNumber) {
     document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    // document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    displayMessage('🎉 Correct Number!');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -35,28 +46,46 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
+  }
+  //GUESS DIFFERENT FROM SECRET NUMBER - REFACTOR
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber
+          ? displayMessage('📈 Guess is too high!')
+          : displayMessage('📉 Guess is too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = 'You Lost the Game 😟';
 
-    //GUESS TOO HIGH
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Guess is too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You Lost the Game 😟';
-      document.querySelector('.score').textContent = 0;
-    }
-    //GUESS TOO LOW
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Guess is too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You Lost the Game 😟';
+      displayMessage('You Lost the Game 😟');
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  //   else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📈 Guess is too high!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'You Lost the Game 😟';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
+
+  //GUESS TOO LOW
+  //   else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📉 Guess is too low!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'You Lost the Game 😟';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
 });
 
 //Reset Logic
@@ -69,5 +98,6 @@ function gameReset() {
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.score').textContent = 20;
   document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  //   document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
 }
