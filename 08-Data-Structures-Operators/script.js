5f30101fe7f21e5723970183bfb73fb421afdd25
Joel Prashanth <142143829+joel-prashanth@users.noreply.github.com>
2025-03-17 17:03:30 +0530

    Finish Destructuring Theory

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -7,51 +7,51 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
   },
 };
 
 // Destructuring arrays
-const arr = [1, 2, 3];
-const [x, y, z] = arr;
-// console.log(x, y, z); // 1 2 3
+// const arr = [1,2,3];
 
-// console.log(restaurant.categories)
-// Destructuring and swapping
-let [main, secondary] = restaurant.categories;
-// console.log(main, secondary); // Italian Pizzeria
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-// Swapping the variables
-[main, secondary] = [secondary, main];
-// console.log(main, secondary); // Pizzeria Italian
+// const [x,y,z] = arr;
+// console.log(x,y,z);
+// console.log(arr);
 
+// const [first, second] = restaurant.categories;
+// console.log(first,second);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary); //Italian Vegetarian
+
+// let temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary); //Vegetarian Italian
+
+//Switching Variables
+
+// [secondary, main] = [main, secondary];
+// console.log(main, secondary); //Italian Vegetarian
+
+//Receive 2 return values from a function
 const [starter, mainCourse] = restaurant.order(2, 0);
-// console.log(starter,mainCourse)
+// console.log(starter, mainCourse);
 
-// Nested destructuring
-const nestedArray = [2, 4, [5, 6]];
+//Nested Destructuring
+const nsetedArray = [2,3,[4,5,6]];
+// const [, ,j] = nsetedArray;  
+const [i, , [j,k,l]] = nsetedArray
+// console.log(j, k, l);
 
-const [i, , [j, k]] = nestedArray;
-// console.log(i,j,k) //2 5 6
+//Default values
 
-// Default values
+const [p=1,q=1,r=1] = [8,9];
+// console.log(p,q,r);
 
-const [p, q = 1, r = 1] = [8];
-console.log(p, q, r);
+
